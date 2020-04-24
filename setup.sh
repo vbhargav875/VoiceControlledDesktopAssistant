93b040e9bd8575846576c699292b20a25c25b0fb
vijaybhargav09 <vbhargav875@gmail.com>
2020-04-25 02:58:26 +0530

    Make changes to setup.sh

--- setup.sh
+++ setup.sh
@@ -7,5 +7,7 @@ sudo add-apt-repository universe && /
 sudo apt install mplayer mplayer-gui && /
 sudo apt install python3-django && /
 sudo apt-get install sqlite3 libsqlite3-dev && /
+sudo apt-get install portaudio19-dev python-pyaudio && /
 sudo pip3 install -r requirements.txt && /
-sudo pip3 install -U PyYAML
+sudo pip3 install -U PyYAML && /
+sudo pip3 install PyAudio
